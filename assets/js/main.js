df069fbe0fb3652fa4d288263c75fddf9e8b4b5a
abothke <andrebothke@gmail.com>
2023-12-28 15:35:55 +0100

    chore: Reformat with Prettier

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,15 @@
 // importieren des Arrays movies aus der Datei movies.js
-import { movies } from './movies.js';
+import { movies } from "./movies.js";
 
 // festlegen des Containers, in dem die Filme angezeigt werden sollen
 const movieContainer = document.getElementById("movie-container");
 
-
 // for-schleife, die für jedes Element des Arrays movies ein Film-Element erstellt und in den Container einfügt
 for (let i = 0; i < movies.length; i++) {
-    const movieDiv = document.createElement("div");
-    let movie = movies[i]
-    movieDiv.className = "movie";
-    movieDiv.innerHTML = `
+  const movieDiv = document.createElement("div");
+  let movie = movies[i];
+  movieDiv.className = "movie";
+  movieDiv.innerHTML = `
         <div class="movie-content">
             <h2>${movie[0]}</h2>
             <p class="movie-year">${movie[1]}</p>
@@ -23,67 +22,70 @@ for (let i = 0; i < movies.length; i++) {
             <p class="movie-rating">${movie[5]}</p>
         </div>
     `;
-    movieContainer.appendChild(movieDiv);
+  movieContainer.appendChild(movieDiv);
 }
 
 // funktion, die die Filme nach dem Titel sortiert und in der richtigen Reihenfolge anzeigt
 const sortMovies = () => {
-    movies.sort();
-    movieContainer.innerHTML = "";
-    for (let i = 0; i < movies.length; i++) {
-        movieContainer.appendChild(movieElement(movies[i]));
-    }
-    return sortMovies
-}
+  movies.sort();
+  movieContainer.innerHTML = "";
+  for (let i = 0; i < movies.length; i++) {
+    movieContainer.appendChild(movieElement(movies[i]));
+  }
+  return sortMovies;
+};
 document.getElementById("alphabet-btn").addEventListener("click", sortMovies);
-
-
-
 
 // Funktion, die die Filme aufsteigend nach Jahr sortiert und darstellt.
 const sortMoviesByYearUp = () => {
-    movies.sort((a, b) => a[1] - b[1]);
-    movieContainer.innerHTML = "";
-    for (let i = 0; i < movies.length; i++) {
-        movieContainer.appendChild(movieElement(movies[i]));
-    }
-    return sortMoviesByYearUp
-}
-document.getElementById("year-up-btn").addEventListener("click", sortMoviesByYearUp);
+  movies.sort((a, b) => a[1] - b[1]);
+  movieContainer.innerHTML = "";
+  for (let i = 0; i < movies.length; i++) {
+    movieContainer.appendChild(movieElement(movies[i]));
+  }
+  return sortMoviesByYearUp;
+};
+document
+  .getElementById("year-up-btn")
+  .addEventListener("click", sortMoviesByYearUp);
 
 // Funktion, die die Filme absteigend nach Jahr sortiert und darstellt.
 const sortMoviesByYearDown = () => {
-    movies.sort((a, b) => b[1] - a[1]);
-    movieContainer.innerHTML = "";
-    for (let i = 0; i < movies.length; i++) {
-        movieContainer.appendChild(movieElement(movies[i]));
-    }
-    return sortMoviesByYearDown
-}
-document.getElementById("year-down-btn").addEventListener("click", sortMoviesByYearDown)
+  movies.sort((a, b) => b[1] - a[1]);
+  movieContainer.innerHTML = "";
+  for (let i = 0; i < movies.length; i++) {
+    movieContainer.appendChild(movieElement(movies[i]));
+  }
+  return sortMoviesByYearDown;
+};
+document
+  .getElementById("year-down-btn")
+  .addEventListener("click", sortMoviesByYearDown);
 
 // funktion, die die Filme nach dem Rating sortiert und in der richtigen Reihenfolge anzeigt
 const sortMoviesByRating = () => {
-    movies.sort((a, b) => b[5] - a[5]);
-    movieContainer.innerHTML = "";
-    for (let i = 0; i < movies.length; i++) {
-        movieContainer.appendChild(movieElement(movies[i]));
-    }
-    return sortMoviesByRating
-}
-document.getElementById("best-rating-btn").addEventListener("click", sortMoviesByRating);
-
-
+  movies.sort((a, b) => b[5] - a[5]);
+  movieContainer.innerHTML = "";
+  for (let i = 0; i < movies.length; i++) {
+    movieContainer.appendChild(movieElement(movies[i]));
+  }
+  return sortMoviesByRating;
+};
+document
+  .getElementById("best-rating-btn")
+  .addEventListener("click", sortMoviesByRating);
 
 // Suchfunktion, die die Filme nach dem Titel durchsucht und die passenden Filme anzeigt
 const searchMovie = () => {
-    const searchInput = document.getElementById("search").value;
-    const searchResult = movies.filter(movie => movie[0].toLowerCase().includes(searchInput.toLowerCase()));
-    movieContainer.innerHTML = "";
-    for (let i = 0; i < searchResult.length; i++) {
-        movieContainer.appendChild(movieElement(searchResult[i]));
-    }
-    return searchMovie
-}
+  const searchInput = document.getElementById("search").value;
+  const searchResult = movies.filter((movie) =>
+    movie[0].toLowerCase().includes(searchInput.toLowerCase())
+  );
+  movieContainer.innerHTML = "";
+  for (let i = 0; i < searchResult.length; i++) {
+    movieContainer.appendChild(movieElement(searchResult[i]));
+  }
+  return searchMovie;
+};
 // Eventlistener, der die Suchfunktion ausführt, sobald eine Taste gedrückt wird, als eine Art Live-Suche
 document.getElementById("search").addEventListener("keyup", searchMovie);
